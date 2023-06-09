cd162a6a355d0447d68bb3f8eed85786c75c9419
Dhruv Saxena <dhruv162002@gmail.com>
2023-06-09 16:13:19 +0530

    Specify models

--- app.py
+++ app.py
@@ -7,13 +7,18 @@ app = Flask(__name__)
 def index():
     return "Question Answering System"
 
-@app.route("/question", methods=['GET', 'POST'])
+@app.route("/question", methods=['POST'])
 def question():
-    qa_model = pipeline("question-answering")
+    # Load the pre-trained question answering model
+    qa_model = pipeline("question-answering", model="distilbert-base-cased-distilled-squad")
+
     text = request.form.get('text')
     question = request.form.get('question')
+
+    # Perform question answering
     result = qa_model(question=question, context=text)
+
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
